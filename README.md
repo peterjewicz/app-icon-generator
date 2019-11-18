00bbe7df93dd4f2ff578fd479526baf56d037226
peterjewicz <peterjewicz@totalwebconnections.com>
2019-11-18 16:42:27 -0600

    add splash support  IOS

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# image-resize
+# App Icon Image Resizer
 
 A simple Clojure application that resizes an image to various dimensions needed for IOS deployment. Currently
 only has dimensions set for app icons.
@@ -9,6 +9,11 @@ First modify the src and output paths in the core file to point to where you wan
 new sizes.
 
 Then, boot up in your repl and move into the ````image-resize.core```` namespace and run ````(resize-image)```` this will take a few seconds and spit out all your images in the output path. You can easily add new sizes or change the names of the output images by modifying the map at the top of the file.
+
+There's also extra maps, paths, and ````resize-splash```` to create splash images. This one is broken into 2 maps for portrait and landscape to help make it look a lot better overall.
+
+## TODO
+1. Android icons
 
 ## License
 
--- src/image_resize/core.clj
+++ src/image_resize/core.clj
@@ -25,9 +25,30 @@
   :icon-83.5-2x.png 167
 })
 
+(def portrait-splash-map {
+  :Default-667h.png `(750 1334)
+  :Default-736h.png `(1242 2208)
+  :iosxportrait.png `(1125 2436)
+  :screen-iphone-portrait-2x.png `(640 960)
+  :screen-iphone-portrait-568h-2x.png `(640 1136)
+  :screen-iphone-portrait.png `(320 480)
+})
+
+(def landscape-splash-map {
+  :screen-ipad-landscape-2x.png `(2048 1536)
+  :screen-ipad-landscape.png `(1024 768)
+  :screen-ipad-portrait-2x.png `(1536 2048)
+  :screen-ipad-portrait.png `(768 1024)
+  :screen-iphone-landscape-2x.png `(960 640)
+  :screen-iphone-landscape.png `(480 320)
+})
+
 ; Change these to match your source and ouput
 (def path-to-src "/Applications/server/image-resize/src/image_resize/icon.png")
 (def path-to-output "/Applications/server/image-resize/src/image_resize/")
+
+(def path-to-src-splash "/Applications/server/image-resize/src/image_resize/landscape-splash.png")
+(def path-to-splash-output "/Applications/server/image-resize/src/image_resize/")
 
 (defn resize-image []
   (for [image image-map]
@@ -37,4 +58,15 @@
         (resize (clojure.java.io/file path-to-src) size size)
         (str path-to-output filename)
         :verbatim))))
+
+; uses force resize so may be a bit wonky - always check after!
+(defn resize-splash [images-to-map]
+  (for [image images-to-map]
+    (let [sizex (first (second image))
+          sizey (second (second image))
+          filename (name (first image))]
+    (as-file
+      (force-resize (clojure.java.io/file path-to-src-splash) sizex sizey)
+      (str path-to-splash-output filename)
+      :verbatim))))
 
